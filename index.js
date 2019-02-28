4c16b27a4680a543f5335b2fba7bd0c591df5b9d
Alyssa Evans <alyssa.m.evans@gmail.com>
2019-02-28 13:15:42 -0800

    date now three separate inputs. updated styling and working on making search wider

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
+const dateFormatter = (day, month, math) => {
+}
+
+
 $('.formy').on('submit', e => {
     e.preventDefault()
-    const $bday = $('#bday').val()
-    console.log('$$$bday: ', $bday);
-    const bday = new Date($bday);
-    console.log({bday});
-    console.log('bday day: ', bday.getDate());
+    const $bday = parseInt($('#bday').val(), 10);
+    const $bmonth = $('#bmonth').val()
+    const $byear = $('#byear').val()
+
+    const trueDate = `${$byear}-${$bmonth}-${$bday}`;
+    const weekBefore = `${$byear}-${$bmonth}-${$bday - 7}`;
+    const weekAfter = `${$byear}-${$bmonth}-${$bday + 7}`
+    console.log({weekBefore});
+    console.log({weekAfter});
+
     $.ajax({
         url: `http://stapi.co/api/v1/rest/episode/search`,
         method: "POST",
         dataType: 'json',
         data: {
-          usAirDateTo: $bday,
-          usAirDateFrom: $bday
+          usAirDateFrom: weekBefore,
+          usAirDateTo: weekAfter
         }
       })
       .then(data => {
-        console.log(typeof data);
-        console.log({data});
-
+        $('.resulty').empty()
         if (!data.episodes.length) {
           $('.resulty').append(`Dammit, Jim! Nothing aired on that day. Check the days before and after`)
           return;
         }
-
+        const perfectFit = data.episodes.find(ep => ep.usAirDate === trueDate)
+        if (perfectFit) {
+          const { series, season, title } = perfectFit;
+          $('.resulty').append(`The stars align! On this date, ${title} was aired. ${series.title} ${season.title}.`)
+          return;
+        }
         const correctAnswer = data.episodes[0];
         const { series, season, title } = correctAnswer;
         $('.resulty').append(`Your result: ${series.title} Season ${season.title}. ${title}`)
