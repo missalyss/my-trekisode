7c3d3d95ae8f8fa7ad555ec579a4de006bf63b3f
Alyssa Evans <alyssa.m.evans@gmail.com>
2019-02-28 21:40:50 -0800

    updated styling and added favicons plus manifest

--- index.js
+++ index.js
@@ -26,18 +26,18 @@ $('.formy').on('submit', e => {
       .then(data => {
         $('.resulty').empty()
         if (!data.episodes.length) {
-          $('.resulty').append(`Dammit, Jim! Nothing aired on that day. Check the days before and after`)
+          $('.resulty').append(`DAMMIT, JIM! Nothing aired on that day. Check the days before and after`)
           return;
         }
         const perfectFit = data.episodes.find(ep => ep.usAirDate === trueDate)
         if (perfectFit) {
           const { series, season, title } = perfectFit;
-          $('.resulty').append(`The stars align! On this date, ${title} was aired. ${series.title} ${season.title}.`)
+          $('.resulty').append(`<p class="enbold">THE STARS ALIGN!</p> <p>On this date, ${title} was aired.</p> <p>${series.title} ${season.title}.</p>`)
           return;
         }
         const correctAnswer = data.episodes[0];
         const { series, season, title } = correctAnswer;
-        $('.resulty').append(`Your result: ${series.title} Season ${season.title}. ${title}`)
+        $('.resulty').append(`<p class="enbold">YOUR RESULT:</p> <p>${series.title}</p> <p>${season.title}</p> <p><span class="enbold">TITLE:</span> ${title}</p>`)
       })
       .catch(err => {
         console.error('red alert! ', err);
